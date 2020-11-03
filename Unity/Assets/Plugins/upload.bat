ecdf25d5fb1adabc7b42628ec630cd751ca0b5f9
mtfallsVR <mtfallsVR@atemosta.com>
2020-11-02 21:33:20 -0500

    Houes of Monkey
    Baking Lights test

--- Unity/Assets/Plugins/upload.bat
+++ Unity/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\Shadow\Documents\Unity\UBW v2\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1580578567635337527.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\Shadow\Documents\GitHub\ASVR-Worlds\Unity\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1597445366997844476.json
