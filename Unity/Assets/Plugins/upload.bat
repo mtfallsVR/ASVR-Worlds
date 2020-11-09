86c9239dcc608c391ebcf4cf6e876448f76f8a7b
mtfallsVR <mtfallsVR@atemosta.com>
2020-11-08 23:41:11 -0500

    Tokyo Subway Part 1

--- Unity/Assets/Plugins/upload.bat
+++ Unity/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\Shadow\Documents\GitHub\ASVR-Worlds\Unity\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1597445366997844476.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\Shadow\Documents\GitHub\ASVR-Worlds\Unity\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1601888516722131775.json
