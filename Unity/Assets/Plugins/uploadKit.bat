ae8f1b7a679466b79e4d2b85d14e2d4c10225855
mtfallsVR <mtfallsVR@atemosta.com>
2020-11-20 14:25:31 -0500

    part3

---
+++ Unity/Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Shadow\Documents\GitHub\ASVR-Worlds\Unity\1610382306566996650_wbr2_kit_assignment.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1610382306566996650.json
